6dd79b98ef498428c8ef4e76e2e80be291cf31ce
hetya <hetya@users.noreply.github.com>
2025-02-03 21:27:46 +0100

    receive packet

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -8,13 +8,17 @@
 # include <sys/types.h>
 # include <sys/socket.h>
 # include <netdb.h>
-#include <arpa/inet.h> // check if needed
-#include <netinet/ip_icmp.h>
+# include <arpa/inet.h> // check if needed
+# include <netinet/ip_icmp.h>
+# include <netinet/ip.h>
+# include <time.h>
 
-// typedef struct s_ping
-// {
-// 	char *hostname;
-// 	struct hostent *hostinfo;
-// } t_ping;
+typedef struct s_ping
+{
+	char *dest_hostname;
+	char *dest_ip;
+    int sockfd;
+    struct icmphdr receive_icmp_header;
+} t_ping;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,12 @@
 
 #include "ft_ping.h"
 
+double get_time_in_ms(){
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000.0 + ts.tv_nsec / 1000000.0;
+}
+
 int	create_socket_and_connect(char *hostname)
 {
 	struct addrinfo	hints;
@@ -89,6 +95,11 @@ int	main(int argc, char **argv)
 	int				socket_fd;
 	struct icmphdr	icmp_header;
 	int				len_received_ip_packet;
+	double			send_time;
+	double			receive_time;
+	int				nb_packets_sent;
+	int				nb_packets_received;
+
 
 	if (getuid() != 0)
 	{
@@ -107,17 +118,24 @@ int	main(int argc, char **argv)
 	icmp_header.un.echo.sequence = 0;
 	icmp_header.checksum = 0;
 	icmp_header.checksum = icmp_checksum(&icmp_header);
-	// data
+	// data + size buf
+	nb_packets_sent = 0;
+	nb_packets_received = 0;
 	while(1)
 	{
-		printf("Packet :\n icmp_type=%d\n icmp_code=%d\n icmp_checksum=%d\n icmp_id=%d\n icmp_seq=%d\n",
-			icmp_header.type, icmp_header.code, icmp_header.checksum, icmp_header.un.echo.id, icmp_header.un.echo.sequence);
+		// recreate the checksum since the sequence number has changed
+		icmp_header.checksum = 0;
+		icmp_header.checksum = icmp_checksum(&icmp_header);
+
+		// printf("Packet :\n icmp_type=%d\n icmp_code=%d\n icmp_checksum=%d\n icmp_id=%d\n icmp_seq=%d\n",
+		// 	icmp_header.type, icmp_header.code, icmp_header.checksum, icmp_header.un.echo.id, icmp_header.un.echo.sequence);
+		send_time = get_time_in_ms();
 		if (send(socket_fd, &icmp_header, sizeof(icmp_header), 0) == -1)
 		{
 			perror("send");
 			return (1);
 		}
-		printf("Sent ICMP packet\n");
+		nb_packets_sent++;
 		char receive_buffer[1024];
 		len_received_ip_packet = recv(socket_fd, receive_buffer, sizeof(receive_buffer), 0);
 		if (len_received_ip_packet == -1)
@@ -125,13 +143,16 @@ int	main(int argc, char **argv)
 			perror("recv");
 			return (1);
 		}
-		printf("Received IP packet: len %d\n");
-		printf("=====\n", len_received_ip_packet);
-		write(1, receive_buffer, len_received_ip_packet);
-		printf("\n=====\n");
-		// printf("Received ICMP packet icmp_seq=%d\n", icmp_header.un.echo.sequence);
-		// icmp_header.un.echo.sequence++;
-		printf("icmp_seq=%d\n", icmp_header.un.echo.sequence);
+		nb_packets_received++;
+		receive_time = get_time_in_ms();
+		struct iphdr *ip_header = (struct iphdr *)receive_buffer;
+		int ip_header_len = ip_header->ihl * 4;
+
+		// ICMP header starts after IP header
+		struct icmphdr *received_icmp_packet = (struct icmphdr *)(receive_buffer + ip_header_len);
+		printf("%d bytes from %s (%s): icmp_seq=%d ttl=%d time=%.1f ms\n",
+			len_received_ip_packet, argv[1], inet_ntoa(*(struct in_addr *)&ip_header->saddr), received_icmp_packet->un.echo.sequence, ip_header->ttl, receive_time - send_time);
+		icmp_header.un.echo.sequence++;
 		sleep(1);
 	}
 	// duplicate package
